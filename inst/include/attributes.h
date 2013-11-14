bdac6172f83a9ec8a819b02d7793e66c3a0896d4
Romain François <romain@r-enthusiasts.com>
2013-11-14 18:15:53 +0100

    factor out AttributesUtil.h

---
+++ inst/include/attributes.h
@@ -0,0 +1,20 @@
+#ifndef ATTRIBUTES_ATTRIBUTES_H
+#define ATTRIBUTES_ATTRIBUTES_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#include <cstring>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+#include <attributes/AttributesUtil.h>
+
+#endif
