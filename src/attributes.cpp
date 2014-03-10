9f29319a14322185530b952181d28577b1ad341c
Romain François <romain@r-enthusiasts.com>
2014-03-10 15:11:33 +0100

    fix parse_cpp_function

--- src/attributes.cpp
+++ src/attributes.cpp
@@ -1,4 +1,3 @@
-
 #define USE_RINTERNALS
 #define R_NO_REMAP
 #include <R.h>
@@ -11,28 +10,31 @@
 
 std::string get_function_signature( SEXP txt, int pos ){
   std::string current_line = CHAR(STRING_ELT(txt, pos)) ;
+  
+  // skip comments
   while( current_line[0] == '/' ){
-    current_line = CHAR(STRING_ELT(txt, ++pos)) ;  
+    current_line = CHAR(STRING_ELT(txt, ++pos)) ;
   }
   
   std::stringstream res ; 
-  res << current_line ;
   while( true ){
-    current_line = CHAR(STRING_ELT(txt, ++pos)) ;
     int brace_pos = current_line.find('{') ;
     if( brace_pos == std::string::npos ){
       res << current_line << ' ' ;
+      current_line = CHAR(STRING_ELT(txt, ++pos)) ;
     } else {
       res << current_line.substr(0, brace_pos) ;
       break ;
     }
+    
   }
+  
   return res.str() ;
   
 }
 
 
-extern "C" SEXP parse_cpp_functions( SEXP txt, SEXP line ){
+extern "C" SEXP parse_cpp_function( SEXP txt, SEXP line ){
   int pos = INTEGER(line)[0] ;
   std::string signature = get_function_signature( txt, pos ) ;
   
